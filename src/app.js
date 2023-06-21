2314f68047168b252296008d9cb92684cf1ca664
malenarost <malerost@outlook.com>
2023-06-21 13:20:07 -0300

    grabo csmbios

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
-import express from "express";
-import { petsRouter } from "./routes/pets.router.js";
-import { usersRouter } from "./routes/users.router.js";
-import { usersHtmlRouter } from "./routes/users.html.router.js";
-import { testSocketChatRouter } from "./routes/test.socket.chat.router.js";
-import handlebars from "express-handlebars";
-import path from "path";
-import { __dirname, connectMongo, connectSocket } from "./utils.js";
+import express from 'express';
+import { petsRouter } from './routes/pets.router.js';
+import { usersRouter } from './routes/users.router.js';
+import { usersHtmlRouter } from './routes/users.html.router.js';
+import { testSocketChatRouter } from './routes/test.socket.chat.router.js';
+import handlebars from 'express-handlebars';
+import path from 'path';
+import { __dirname, connectMongo, connectSocket } from './utils.js';
 const app = express();
-const port = 3000;
+const port = 8080;
 
 const httpServer = app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}`);
@@ -19,27 +19,27 @@ connectSocket(httpServer);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.engine("handlebars", handlebars.engine());
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "handlebars");
+app.engine('handlebars', handlebars.engine());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars');
 
 // app.use(express.static("public"));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Rutas: API REST CON JSON
-app.use("/api/users", usersRouter);
-app.use("/api/pets", petsRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/pets', petsRouter);
 
 //Rutas: HTML RENDER SERVER SIDE
-app.use("/users", usersHtmlRouter);
+app.use('/users', usersHtmlRouter);
 
 //Rutas: SOCKETS
-app.use("/test-chat", testSocketChatRouter);
+app.use('/test-chat', testSocketChatRouter);
 
-app.get("*", (req, res) => {
+app.get('*', (req, res) => {
   return res.status(404).json({
-    status: "error",
-    msg: "no encontrado",
+    status: 'error',
+    msg: 'no encontrado',
     data: {},
   });
 });
