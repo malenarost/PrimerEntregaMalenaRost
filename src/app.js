26b1ebc22ade1e7af784185e1e58b3bc46afa8ae
malenarost <malerost@outlook.com>
2023-07-01 22:35:20 -0300

    grabo cambios

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-//@ts-check
+import cookieParser from 'cookie-parser';
+
 import express from 'express';
 import handlebars from 'express-handlebars';
 import { Server } from 'socket.io';
@@ -6,10 +7,11 @@ import { productManager } from './manager/productManager.js';
 import { cartsRoute } from './routes/carts.routes.js';
 import { productsRoute } from './routes/product.routes.js';
 import { realTimeProducts } from './routes/real-time-products.routes.js';
-// @ts-ignore
-import { __dirname } from './dirname.js';
-import { connectMongo } from './utils/connections.js';
 import { viewRouter } from './routes/views.routes.js';
+// @ts-ignore
+import { __dirname } from './dirName.js';
+import { connectMongo } from './utils/connections.js';
+import session from 'express-session';
 const app = express();
 const port = 8080;
 
@@ -17,7 +19,15 @@ connectMongo();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cookieParser('coder-secreto'));
+app.use(
+  session({
+    secret: 'un-re-secreto',
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: true },
+  })
+);
 // CONFIGURACION DEL MOTORO DE HANDLEBARS
 app.engine('handlebars', handlebars.engine());
 app.set('views', __dirname + '/views');
@@ -38,6 +48,50 @@ app.use('/', viewRouter);
 
 /* VISTA */
 app.use('/realtimeproducts', realTimeProducts);
+
+////////////////////////////sessions/////////////////////////
+
+app.get('/session', (req, res) => {
+  if (req.session.cont) {
+    res.session.cont++;
+    res.send('nos visitaste' + req.session.cont);
+  } else {
+    req.session.cont = 1;
+    req.session.nombre = 'juan';
+    req.session.isAdmin = true;
+    res.send('nos visitaste' + 1);
+  }
+});
+
+app.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.json({ status: 'logout ERROR', body: err });
+    }
+    res.send('logout OK!');
+  });
+});
+/////////////////////Cookies///////////////////////
+
+/*app.get('/setCookie', (req, res) => {
+  res.cookie('cookie-test1', ' dato1', { maxAge: 10000, signed: true });
+  res.cookie('cookie-test2', ' dato2', { maxAge: 10000, signed: true });
+  res.cookie('cookie-test3', ' dato3', { maxAge: 10000, signed: true });
+  return res.json({ msg: 'cookie puesta' });
+});
+
+app.get('/getCookies', (req, res) => {
+  console.log('cookies', req.cookies);
+  console.log('signedCookies', req.signedCookies);
+  res.send('nada');
+});
+
+app.get('/deleteCookie', (req, res) => {
+  res.clearCookie('cookie-test1');
+  res.send('borrado');
+});*/
+
+/////////////////////////////
 
 app.get('*', (req, res) => {
   return res.status(404).json({
